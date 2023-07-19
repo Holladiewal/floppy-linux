1ef78d88aaa85f0a8bce8bee2f83af2ab3d31edc
Holladiewal <holladiewal@whydoyouhate.me>
2023-07-19 09:28:44 +0200

    Make flmount.c be proper
    It worked before but now we actually include sys/mount.h and use the Constants.
    
    Before we just used and hoped that gcc and it's linker found what we meant

--- src/flmount/mount.c
+++ src/flmount/mount.c
@@ -3,17 +3,18 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
+#include <sys/mount.h>
 
 //#define DEBUG
 #ifndef DEBUG
-int puts(const char * __s){return '\0';}
+int puts(const char * __s){return 0;}
 #endif
 
-int main(int argc, char* const argv[]){
+int main(int argc, char* argv[]){
 	puts("Insert system disk 2 into the B: drive. Press ENTER to continue.");
 	getchar();
 	mount("none", "/dev", "devtmpfs", 0, 0);
-	mount("/dev/fd1", "/mnt/floppy", "squashfs", 1, 0);
+	mount("/dev/fd1", "/mnt/floppy", "squashfs", MS_RDONLY, 0);
 	puts("Mounted floppy!");
 	if(!fork()){
 		// We are the child!
@@ -21,15 +22,10 @@ int main(int argc, char* const argv[]){
 		execl("/mnt/floppy/bin/busybox", "/mnt/floppy/bin/busybox", "cp", "-r", "/mnt/floppy/bin", "/mnt/floppy/sbin", "/mnt/floppy/usr", "/mnt/floppy/etc", "/", 0);
 		puts("Copied coreutils from floppy");
 	} else {
-		// We are the parent or we totally failed. But we assume we are awesome!
-
-		char* betterargv[argc];
-		betterargv[0] = "/sbin/init";
-		for (int i = 1; i < argc; ++i){
-			betterargv[i] = argv[i];
-		}
+		// We are either the parent, or we totally failed. But we assume we are awesome!
+		argv[0] = "/sbin/init";
 		wait(0); // Just wait for the child to terminate
 		puts("Handing off to /sbin/init");
-		execv("/sbin/init", betterargv); // And handoff to init
+		execv("/sbin/init", argv); // And handoff to init
 	}
 }
